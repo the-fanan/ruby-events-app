285b6f6dc500310bc1ab8f481ce8fdf5d4bb1b3c
Fanan Dala <fanan.dala@yahoo.com>
2021-07-04 00:18:50 +0100

    chore: add scaffold for events and tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tickets
+  resources :events
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
