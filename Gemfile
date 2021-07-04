15eb6419fe0bdbfae69ee8ada0c12fdec431c3fb
Fanan Dala <fanan.dala@yahoo.com>
2021-07-04 23:03:24 +0100

    test: add sign in for testing auth routes

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ end
 
 group :development, :test do 
   gem 'rexml'
+  gem 'factory_bot_rails'
   gem "rspec-rails"
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :view
   config.include Devise::Test::ControllerHelpers, :type => :view
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Warden::Test::Helpers, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
